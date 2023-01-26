74ade572acc34d76e4973716215b92e9241aa126
sasha v <volfym79@gmail.com>
2023-01-26 17:07:25 +0100

    exercise 3.22

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 
-const Number = require('./models/number')
-const {req, res, response} = require('express')
+const { response } = require('express')
 const express = require('express')
 const cors = require('cors')
+const NumEntry = require('./models/number')
+
 const app = express()
 
 // middleware
@@ -12,123 +13,107 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-{// Logging, prod
-
-/* let morgan;
-if (process.env.NODE_ENV !== 'production') {
-    morgan = require('morgan')
-    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requested'))
-    morgan.token('requested', (reqS, resS, param) => {
-        return ''
-    })
-} */}
-
 // RESTful/CRUD
 
-app.get('/info', (req, res) => {
-    Number.find({})
-        .then(numbers => {
-            res.send(`Phonebook has info on ${numbers.length} people. <br>${new Date()}`)
-        })
-}) 
+app.get('/info', (_req, res) => {
+  NumEntry.find({})
+    .then((numbers) => {
+      res.send(`Phonebook has info on ${numbers.length} people. <br>${new Date()}`)
+    })
+})
 
-app.get('/api/persons', (req, res) => {
-    Number.find({})
-        .then(numbers => {
-            res.json(numbers)
-        })
+app.get('/api/persons', (_req, res) => {
+  NumEntry.find({})
+    .then((numbers) => {
+      res.json(numbers)
+    })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Number.findById(req.params.id)
-        .then(number => {
-            if(number) {
-                res.json(number)
-            }
-            else {
-                res.status(404).end()
-            }
-        })
-        // CAST ERROR
-        .catch(error => next(error))
+  NumEntry.findById(req.params.id)
+    .then((number) => {
+      if (number) {
+        res.json(number)
+      } else {
+        res.status(404).end()
+      }
+    })
+    // CAST ERROR
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const { name, number } = req.body
 
-    // SAVING
+  // SAVING
 
-    const newEntry = new Number({
-        name: body.name,
-        number: body.number,
+  const newEntry = new NumEntry({ name, number })
+
+  NumEntry
+    .find({ name: newEntry.name })
+    .then((n) => {
+      if (n.length) {
+        res.status(400).send({ error: 'Name already in phonebook' })
+      } else {
+        newEntry
+          .save()
+          .then((saved) => {
+            res.json(saved)
+          })
+          .catch((error) => next(error))
+      }
     })
-
-    Number.find({name : newEntry.name})
-        .then(n => {
-            if (n.length) {
-                res.status(400).send({error: 'Name already in phonebook'})
-            } else {
-                newEntry.save().then(saved => {
-                    res.json(saved)
-                })
-                .catch(error => next(error))
-            }
-        })
-    
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
-    
-    const updatedEntry = {
-        name: body.name,
-        number: body.number
-    }
+  const { name, number } = req.body
 
-    Number.findByIdAndUpdate(req.params.id, updatedEntry, {new: true, runValidators: true, context: 'query'} )
-        .then(updated => {
-            res.json(updated)
-        })
-        .catch(error => next(error))
+  NumEntry.findByIdAndUpdate(req.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    .then((updated) => {
+      res.json(updated)
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Number.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  NumEntry.findByIdAndRemove(req.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((_result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 // unknown endpoints
 
-const unknownEndpoint = (req, res) => {
-    response.status(404).send({ error: 'unknown endpoint'})
+// eslint-disable-next-line no-unused-vars
+const unknownEndpoint = (_req, _res) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-
 // ERROR HANDLER
 
-const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-    
-    if (error.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json(error.message)
-    }
+// eslint-disable-next-line consistent-return
+const errorHandler = (error, _req, res, next) => {
+  console.error(error.message)
 
-    next(error)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return res.status(400).json(error.message)
+  }
+
+  next(error)
 }
 
 app.use(errorHandler)
 
-
 // LISTEN
 
-const PORT = process.env.PORT 
+const { PORT } = process.env
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- phonebook/models/number.js
+++ phonebook/models/number.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
@@ -7,29 +9,27 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const numberSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => /^\d{2}-\d{6,}$|^\d{3}-\d{5,}$|^\d{8,}$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number.`,
     },
-    number: {
-        type: String,
-        validate: {
-            validator: (v) => {
-                return /^\d{2}-\d{6,}$|^\d{3}-\d{5,}$|^\d{8,}$/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number.`
-        },
-        required: true
-    }
+    required: true,
+  },
 })
 
 numberSchema.set('toJSON', {
-    transform: (document, returned) => {
-        returned.id = returned._id.toString()
-        delete returned._id
-        delete returned.__v
-    }
+  transform: (document, returned) => {
+    returned.id = returned._id.toString()
+    delete returned._id
+    delete returned.__v
+  },
 })
 
 module.exports = mongoose.model('Number', numberSchema)
